8218baac90679e259a58f4266620d4c287cef998
Pineapple217 <rosierjules@gmail.com>
2024-01-20 21:41:26 +0100

    performance improvements

--- view/utils.go
+++ view/utils.go
@@ -16,10 +16,18 @@ import (
 	"github.com/gomarkdown/markdown/parser"
 )
 
+var (
+	reS      *regexp.Regexp = regexp.MustCompile(`https?://open\.spotify\.com/track/(\S+)`)
+	reY      *regexp.Regexp = regexp.MustCompile(`https?://(?:www\.)?youtu(?:be\.com/watch\?v=)|(?:\.be/)(\S+)`)
+	reYTID   *regexp.Regexp = regexp.MustCompile(`(?:youtube\.com\/watch\?v=|youtu\.be\/)([^&?/]+)`)
+	reSID    *regexp.Regexp = regexp.MustCompile(`/track/(\w+)`)
+	timezone *time.Location = initTimezone()
+	renderer *html.Renderer = initRender()
+)
+
 func renderSpotifyEmbed(ctx context.Context, w io.Writer, l *ast.Link, entering bool) {
 	if entering {
-		re := regexp.MustCompile(`/track/(\w+)`)
-		id := re.FindStringSubmatch(string(l.Destination))[1]
+		id := reSID.FindStringSubmatch(string(l.Destination))[1]
 		queries := database.GetQueries()
 		sc, err := queries.GetSpotifyCache(ctx, id)
 		if err != nil {
@@ -39,8 +47,7 @@ func renderSpotifyEmbed(ctx context.Context, w io.Writer, l *ast.Link, entering 
 
 func renderYoutubeEmbed(ctx context.Context, w io.Writer, l *ast.Link, entering bool) {
 	if entering {
-		re := regexp.MustCompile(`(?:youtube\.com\/watch\?v=|youtu\.be\/)([^&?/]+)`)
-		id := re.FindStringSubmatch(string(l.Destination))[1]
+		id := reYTID.FindStringSubmatch(string(l.Destination))[1]
 		queries := database.GetQueries()
 		ytc, err := queries.GetYoutubeCache(ctx, id)
 		if err != nil {
@@ -60,12 +67,10 @@ func renderYoutubeEmbed(ctx context.Context, w io.Writer, l *ast.Link, entering 
 func makeEmbedRenderHook(ctx context.Context) html.RenderNodeFunc {
 	return func(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
 		if link, ok := node.(*ast.Link); ok {
-			reS := regexp.MustCompile(`https?://open\.spotify\.com/track/(\S+)`)
 			if reS.MatchString(string(link.Destination)) {
 				renderSpotifyEmbed(ctx, w, link, entering)
 				return ast.GoToNext, true
 			}
-			reY := regexp.MustCompile(`https?://(?:www\.)?youtu(?:be\.com/watch\?v=)|(?:\.be/)(\S+)`)
 			if reY.MatchString(string(link.Destination)) {
 				renderYoutubeEmbed(ctx, w, link, entering)
 				return ast.GoToNext, true
@@ -77,32 +82,40 @@ func makeEmbedRenderHook(ctx context.Context) html.RenderNodeFunc {
 
 func MdToHTML(ctx context.Context, md string) string {
 	// create markdown parser with extensions
+	// TODO: make a gobal parser 1 time
 	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock | parser.FencedCode
 	p := parser.NewWithExtensions(extensions)
 	doc := p.Parse([]byte(md))
 
-	// create HTML renderer with extensions
-	htmlFlags := html.CommonFlags | html.HrefTargetBlank
-	opts := html.RendererOptions{
-		Flags:          htmlFlags,
-		RenderNodeHook: makeEmbedRenderHook(ctx),
-	}
-	renderer := html.NewRenderer(opts)
+	renderer.Opts.RenderNodeHook = makeEmbedRenderHook(ctx)
 
 	// TODO: syntax highlighter with github.com/alecthomas/chroma
 	// https://blog.kowalczyk.info/article/cxn3/advanced-markdown-processing-in-go.html
 	return string(markdown.Render(doc, renderer))
 }
 
+func initRender() *html.Renderer {
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{
+		Flags: htmlFlags,
+	}
+	return html.NewRenderer(opts)
+
+}
+
 func UnixTimeToHTMLDateTime(unixTime int64) string {
+	goTime := time.Unix(unixTime, 0).In(timezone)
+	formattedTime := goTime.Format("2006-01-02T15:04:05.000Z")
+	htmlDateTime := fmt.Sprintf(`<time datetime="%s">%s</time>`, formattedTime, goTime.Format("Mon, 2 Jan 2006 15:04:05 MST"))
+
+	return htmlDateTime
+}
+
+func initTimezone() *time.Location {
 	// TODO: make it an env
 	loc, err := time.LoadLocation("Europe/Brussels")
 	if err != nil {
 		loc = time.UTC
 	}
-	goTime := time.Unix(unixTime, 0).In(loc)
-	formattedTime := goTime.Format("2006-01-02T15:04:05.000Z")
-	htmlDateTime := fmt.Sprintf(`<time datetime="%s">%s</time>`, formattedTime, goTime.Format("Mon, 2 Jan 2006 15:04:05 MST"))
-
-	return htmlDateTime
+	return loc
 }
