cfa9604f2b9e3f2eb8b4a1a6beb0dccdbbcb7bf4
Pineapple217 <rosierjules@gmail.com>
2024-10-18 21:11:08 +0200

    Small refactor

--- pkg/view/post_templ.go
+++ pkg/view/post_templ.go
@@ -389,16 +389,9 @@ func Posts(posts []database.Post, tags []database.GetAllTagsRow, nav templ.Compo
 				return templ_7745c5c3_Err
 			}
 			for _, post := range posts {
-				if post.CreatedAt == selected {
-					templ_7745c5c3_Err = postBase(post, q, true).Render(ctx, templ_7745c5c3_Buffer)
-					if templ_7745c5c3_Err != nil {
-						return templ_7745c5c3_Err
-					}
-				} else {
-					templ_7745c5c3_Err = postBase(post, q, false).Render(ctx, templ_7745c5c3_Buffer)
-					if templ_7745c5c3_Err != nil {
-						return templ_7745c5c3_Err
-					}
+				templ_7745c5c3_Err = postBase(post, q, post.CreatedAt == selected).Render(ctx, templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
 				}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(" ")
