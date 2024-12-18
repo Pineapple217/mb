d475374d4ed6788eefbf41d2b8e45ef4a453d1ff
Jules Rosier <rosierjules@gmail.com>
2024-12-18 18:55:18 +0100

    manifest caching

--- pkg/handler/manifest.go
+++ pkg/handler/manifest.go
@@ -18,18 +18,19 @@ type IconSet struct {
 	Icons []Icon `json:"icons"`
 }
 
+var manifest = IconSet{
+	Icons: []Icon{
+		{Src: s.StaticMap["/static/icon-192.png"], Type: "image/png", Sizes: "192x192"},
+		{Src: s.StaticMap["/static/icon-512.png"], Type: "image/png", Sizes: "512x512"},
+	},
+}
+
 func (h *Handler) Manifest(c echo.Context) error {
-	// TODO: could cache the man
-	var manifest = IconSet{
-		Icons: []Icon{
-			{Src: s.StaticMap["/static/icon-192.png"], Type: "image/png", Sizes: "192x192"},
-			{Src: s.StaticMap["/static/icon-512.png"], Type: "image/png", Sizes: "512x512"},
-		},
-	}
 	jsonData, err := json.Marshal(manifest)
 	if err != nil {
 		return err
 	}
 	c.Response().Header().Set("Content-Type", "application/manifest+json")
+	c.Response().Header().Add("Cache-Control", "public, max-age=3600, must-revalidate")
 	return c.String(http.StatusOK, string(jsonData))
 }
